ed2214cb7f3abef35a64f0288205ccd1734198fa
coastalwhite <me@gburghoorn.com>
2025-10-13 16:00:50 +0200

    perf: Implement native `null_count`, `any` and `all` group-by aggregations
    This implements native aggregations for `Expr.null_count`, `Expr.any` and
    `Expr.all`.
    
    This can lead to massive memory and speed improvements, because of two reasons:
    
    1. Data does not have be aggregated beforehand.
    2. Aggregations can run in parallel without having to clone unnecessarily.
    
    In microbenchmarks, this could speed `lf.group_by(...).agg(...)` and
    `{list,arr}.{eval,agg}(...)` up by anywhere between 5x - 1000x.
    
    This is also in preparation for replacing `list.{any,all}()` by
    `list.agg(pl.element().{any,all}())`.

--- crates/polars-core/src/frame/column/mod.rs
+++ crates/polars-core/src/frame/column/mod.rs
@@ -1,6 +1,6 @@
 use std::borrow::Cow;
 
-use arrow::bitmap::BitmapBuilder;
+use arrow::bitmap::{Bitmap, BitmapBuilder};
 use arrow::trusted_len::TrustMyLength;
 use num_traits::{Num, NumCast};
 use polars_compute::rolling::QuantileMethod;
@@ -1902,6 +1902,11 @@ impl Column {
         self.as_materialized_series()
             .propagate_nulls()
             .map(Column::from)
+    }
+
+    pub fn rechunk_validity(&self) -> Option<Bitmap> {
+        // @scalar-opt
+        self.as_materialized_series().rechunk_validity()
     }
 }
 
